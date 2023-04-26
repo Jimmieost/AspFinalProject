1e63bd2aa63a75b6ff892798ccb0cd50cb1648cf
Jimmieost <jimmie.ost@utb.ecutbildning.se>
2023-04-26 14:59:02 +0200

    Added some to startup project

--- AspFinalProject/Program.cs
+++ AspFinalProject/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using AspFinalProject.Models.Contexts;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<IdentityContext>(x => x.UseSqlServer(""));
 
 
 
